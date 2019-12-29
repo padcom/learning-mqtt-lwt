ce2aecafdea4aee5af8031205191814f139cf71f
Maciej Hryniszak <maciej.hryniszak@consultant.lego.com>
2019-12-29 16:11:56 +0100

    Initial import

---
+++ client.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+const mqtt = require('mqtt').connect('mqtt://127.0.0.1', {
+  codec: 'raw',
+  keepalive: 1,
+  will: { topic: 'client-001/status', payload: 'offline' }
+})
+
+mqtt.on('connect', () => {
+  console.log('MQTT connected')
+  mqtt.publish('client-001/status', 'online')
+})
+
+setInterval(() => {
+  let c = 0
+  for (i = 0; i < 10000; i++) for (j = 0; j < 10000; j++) c++
+  console.log(c)
+}, 500)
---
+++ sniffer.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+const mqtt = require('mqtt').connect('mqtt://127.0.0.1', { codec: 'raw' })
+
+mqtt.on('message', (topic, value) => {
+  console.log(topic, value.toString())
+})
+
+mqtt.on('connect', () => {
+  console.log('MQTT Connected')
+  mqtt.subscribe('client-001/#')
+})
